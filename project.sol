7340e8407d7662d8f15453db0325f9859a888c25
Arush Tripathi <139771140+arushtripathi121@users.noreply.github.com>
2024-07-09 13:08:09 +0530

    Update project.sol

--- project.sol
+++ project.sol
@@ -3,18 +3,44 @@ pragma solidity ^0.8.7;
 
 contract MyContract {
 
-    function testRequire(uint _eth) public pure {
-        require(_eth > 10000, "eth must be greater than 10000");
-    }
+    uint public percentageObtained;
+    string public resultMessage;
+    string public grade;
 
-    function testRevert(uint _eth) public pure{
-        if(_eth < 10000){
-            revert("eth must be greater to less than 10000");
+    function calculatePercentage(uint _obtainedMarks, uint _totalMarks) public {
+        require(_totalMarks > 0, "Total marks must be greater than zero");
+        require(_obtainedMarks <= _totalMarks, "Obtained marks must not exceed total marks");
+
+       
+        percentageObtained = (_obtainedMarks * 100) / _totalMarks;
+
+        if (percentageObtained < 30) {
+            resultMessage = "fail";
+        } else {
+            resultMessage = "pass";
         }
     }
 
-    uint number;
-    function testAssert() public view{
-        assert(number == 0);
+    function generateResult() public {
+        require(percentageObtained <= 100, "Something went wrong, calculate the percentage first");
+        assert(percentageObtained >= 0 && percentageObtained <= 100);
+
+        if (percentageObtained >= 90) {
+            grade = "A+";
+        } else if (percentageObtained >= 80) {
+            grade = "A";
+        } else if (percentageObtained >= 70) {
+            grade = "B+";
+        } else if (percentageObtained >= 60) {
+            grade = "B";
+        } else if (percentageObtained >= 50) {
+            grade = "C+";
+        } else if (percentageObtained >= 40) {
+            grade = "C";
+        } else if (percentageObtained >= 30) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
     }
 }
